14d0bd72b1e71a0a224c0871127b3c15e5058ec0
Filip Lindbladh <filiplindbladhh@gmail.com>
2018-06-28 11:04:12 +0200

    moved positions content to wordpress and refactored positions component

--- src/components/Positions.vue
+++ src/components/Positions.vue
@@ -1,34 +1,32 @@
 <template>
-    <div id="positions">
-        <!-- Header  -->
-        <lrMenu></lrMenu>
-        <div class="positionsHeader overlay">
-            <h3 class="responsiveHeading">Jobba hos oss!</h3>
-        </div>
-        <div class="positionsInfo container">
-            <div>
-                <p class="mainText">
-                    <span>Vi bryr oss om</span> vår personal och vill att alla medarbetare ska känna sig delaktiga i Byråns verksamhet och utveckling. Vill du också arbeta på en framåtsträvande byrå med en stark teamkänsla? Vi är alltid i behov av kompetent personal och tar gärna emot intresseanmälningar.
-                </p>
-            </div>
-            <div>
-                <img src="../img/LRtable.jpg" alt="Picture of LR Örebro staff">
-            </div>
-        </div>
-        <div class="work">
-            <i class="fa fa-briefcase" aria-hidden="true"></i>
-            <h3>Just nu söker vi en revisorsassistent</h3>
-            <p class="leadText">
-                Som revisorsassistent hos oss får du en aktiv roll i ditt arbete där du får arbeta med samtliga delar i en revision, som planering, revidering och rapportering. Våra kunder är främst små- och medelstora ägarledda bolag i en mängd olika branscher vilket ger ett omväxlande och intressant arbete. Vi söker främst dig som arbetat på en revisionsbyrå och har några års erfarenhet men nu söker en ny utmaning!
-            </p>
-            <a href="mailto:hej@lr-orebro.se" class="button is-primary is-outlined">
-            SKICKA IN ANSÖKAN
-            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-            </a>
-        </div>
-        <!-- Footer here -->
-        <lrFooter></lrFooter>
+  <div id="positions">
+    <!-- Header  -->
+    <lrMenu></lrMenu>
+    <div class="positionsHeader overlay">
+      <h3 class="responsiveHeading"> {{ this.workTexts.heading }}</h3>
     </div>
+    <div class="positionsInfo container">
+      <div>
+        <p class="mainText" v-html="this.workTexts.text">
+        </p>
+      </div>
+      <div>
+        <img src="../img/LRtable.jpg" alt="Picture of LR Örebro staff">
+      </div>
+    </div>
+    <div class="work-banner" v-if="this.workTexts.showBanner">
+      <i class="fa fa-briefcase" aria-hidden="true"></i>
+      <h3>{{ this.workTexts.bannerHeading }}</h3>
+      <p class="leadText" v-html="this.workTexts.bannerText">
+      </p>
+      <a href="mailto:hej@lr-orebro.se" class="button is-primary is-outlined">
+      SKICKA IN ANSÖKAN
+      <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+      </a>
+    </div>
+    <!-- Footer here -->
+    <lrFooter></lrFooter>
+  </div>
 </template>
 
 <script>
@@ -40,130 +38,124 @@ export default {
   components: {
     lrMenu,
     lrFooter
+  },
+  computed: {
+    pages () {
+      return this.$store.state.pages
+    },
+    workTexts () {
+      if (this.pages.length > 0) {
+        return {
+          heading: this.pages[0].acf.heading,
+          text: this.pages[0].acf.text,
+          showBanner: this.pages[0].acf.show_banner,
+          bannerHeading: this.pages[0].acf.banner_heading,
+          bannerText: this.pages[0].acf.banner_text
+        }
+      } else {
+        return {
+          heading: '...',
+          text: '...',
+          banner: '',
+          bannerHeading: '...',
+          bannerText: '...'
+        }
+      }
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-    .positionsHeader {
-        margin-top:67px;
-        background-image: url("../img/positionsHeader.png");
-        background-repeat: no-repeat;
-        background-position:center;
-        background-size:cover;
-        width:100%;
-        height:250px;
-        position:relative;
-        z-index:-1;
-        display:-webkit-box;
-        display:-ms-flexbox;
-        display:flex;
-        -webkit-box-pack:center;
-            -ms-flex-pack:center;
-                justify-content:center;
-        -webkit-box-align:center;
-            -ms-flex-align:center;
-                align-items:center;
-        h3 {
-            position:relative;
-            color:$white;
-            z-index:450;
-            font-size:35px;
-        }
+.positionsHeader {
+  margin-top:67px;
+  background-image: url("../img/positionsHeader.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 250px;
+  position: relative;
+  z-index: -1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h3 {
+    position: relative;
+    color: $white;
+    z-index: 450;
+    font-size: 35px;
+  }
+}
+.positionsInfo {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  padding: 40px;
+  width: 100%;
+  div {
+    width: 50%;
+    flex-basis: 50%;
+    img {
+      margin-top: 20px;
     }
-    .positionsInfo {
-        display:-webkit-box;
-        display:-ms-flexbox;
-        display:flex;
-        -webkit-box-orient:horizontal;
-        -webkit-box-direction:normal;
-        -ms-flex-direction:row;
-                flex-direction:row;
-        -webkit-box-pack: center;
-        -ms-flex-pack: center;
-                justify-content: center;
-        -webkit-box-align:center;
-        -ms-flex-align:center;
-                align-items:center;
-        margin:0 auto;
-        padding:40px;
-        width:100%;
-        div {
-            width: 50%;
-            -ms-flex-preferred-size: 50%;
-                flex-basis: 50%;
-            img {
-                margin-top: 20px;
-            }
-        }
-        .mainText {
-            width:95%;
-            text-align:left;
-        }
-        .lrHeaderLine {
-            background-color: $red;
-        }
-        span {
-            font-weight: bold;
-            font-size: 20px;
-        }
+  }
+  .mainText {
+    width: 95%;
+    text-align: left;
+  }
+  .lrHeaderLine {
+    background-color: $red;
+  }
+  span {
+    font-weight: bold;
+    font-size: 20px;
+  }
+}
+.work-banner {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: $grey;
+  width: 100%;
+  height: auto;
+  padding: 20px;
+  .fa-briefcase {
+    font-size: 30px;
+  }
+  .leadText {
+    width: 70%;
+    text-align: center;
+    margin: 20px 0 20px 0;
+  }
+  span {
+    font-weight: 900;
+  }
+  i {
+    margin-left: 5px;
+  }
+  a {
+    z-index:999;
+  }
+}
+@media screen and (max-width: $tablet - 1px) {
+  .positionsInfo {
+    flex-direction: column;
+    div {
+      width: 100%;
+      flex-basis: 50%;
     }
-    .work {
-        display:-webkit-box;
-        display:-ms-flexbox;
-        display:flex;
-        -webkit-box-orient:vertical;
-        -webkit-box-direction:normal;
-            -ms-flex-direction:column;
-                flex-direction:column;
-        -webkit-box-pack:center;
-            -ms-flex-pack:center;
-                justify-content:center;
-        -webkit-box-align:center;
-            -ms-flex-align:center;
-                align-items:center;
-        background-color: $grey;
-        width: 100%;
-        height: auto;
-        padding:20px;
-        .fa-briefcase {
-            font-size: 30px;
-        }
-        .leadText {
-            width:70%;
-            text-align:center;
-            margin:20px 0 20px 0;
-        }
-        span {
-            font-weight:900;
-        }
-        i {
-            margin-left:5px;
-        }
-        a {
-            z-index:999;
-            }
+  }
+  .work {
+    .leadText {
+      width: 100%;
     }
-    @media screen and (max-width: $tablet - 1px) {
-    .positionsInfo {
-        -webkit-box-orient:vertical;
-        -webkit-box-direction:normal;
-            -ms-flex-direction:column;
-                flex-direction:column;
-            div {
-                width: 100%;
-                /* margin-top:20px;   */
-                -ms-flex-preferred-size: 50%;
-                    flex-basis: 50%;
-            }
-        }
-        .work {
-            .leadText {
-                width:100%;
-            }
-            h3 {
-                text-align:center;
-            }
-        }
+    h3 {
+      text-align: center;
     }
+  }
+}
 </style>
