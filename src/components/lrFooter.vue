8b32f656ea9ee9ce7ba9a7a1cbd2da652722383a
Filip Lindbladh <filip.lindbladh@beamonpeople.se>
2020-12-07 21:16:38 +0100

    update newsletters and header

--- src/components/lrFooter.vue
+++ src/components/lrFooter.vue
@@ -1,45 +1,60 @@
 <template>
-    <div class="lrFooter">
-      <div class="footer">
-        <div class="container footer-container">
-          <div class="footer-item">
-            <h4>ADRESS</h4>
-            <hr class="lrHeaderLine">
-            <p>{{ this.footerTexts.adress }}</p>
-          </div>
-          <div class="footer-item">
-            <h4>KONTAKT</h4>
-            <hr class="lrHeaderLine">
-            <ul>
-              <li><a :href="'tel:' + this.footerTexts.vaxel">{{ this.footerTexts.vaxel }}</a></li>
-              <li><a :href="'mailto:' + this.footerTexts.epost"> {{ this.footerTexts.epost }}</a></li>
-            </ul>
-          </div>
-          <div class="footer-item">
-            <h4>AKTUELLT</h4>
-            <hr class="lrHeaderLine">
-            <ul>
-              <li><a href="static/news/Nyhetsbrev_Nr34-2017.pdf" target="_blank">Nyhetsbrev December 2017</a></li>
-              <li><a href="static/news/Nyhetsbrev-Nr2-2017.pdf" target="_blank">Nyhetsbrev Juni 2017</a></li>
-              <li><a href="static/news/Nyhetsbrev-Nr-1-2017.pdf" target="_blank">Nyhetsbrev April 2017</a></li>
-            </ul>
-          </div>
+  <div class="lrFooter">
+    <div class="footer">
+      <div class="container footer-container">
+        <div class="footer-item">
+          <h4>ADRESS</h4>
+          <hr class="lrHeaderLine" />
+          <p>{{ this.footerTexts.adress }}</p>
+        </div>
+        <div class="footer-item">
+          <h4>KONTAKT</h4>
+          <hr class="lrHeaderLine" />
+          <ul>
+            <li>
+              <a :href="'tel:' + this.footerTexts.vaxel">{{ this.footerTexts.vaxel }}</a>
+            </li>
+            <li>
+              <a :href="'mailto:' + this.footerTexts.epost">{{ this.footerTexts.epost }}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer-item">
+          <h4>AKTUELLT</h4>
+          <hr class="lrHeaderLine" />
+          <ul>
+            <li>
+              <a
+                href="static/news/Nyhetsbrev_N1-2020_centralt.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Nyhetsbrev April 2020</a>
+            </li>
+            <li>
+              <a
+                href="static/news/ExtraNyhetsbrev20200414.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >Extra Nyhetsbrev gällande Coronapandemin</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div class="footer-icon-container">
-        <img class="footer-icon" src="../img/footer.svg" alt="footer-icon.svg">
-      </div>
     </div>
+    <div class="footer-icon-container">
+      <img class="footer-icon" src="../img/footer.svg" alt="footer-icon.svg" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'lrFooter',
   computed: {
-    pages () {
+    pages() {
       return this.$store.state.pages
     },
-    footerTexts () {
+    footerTexts() {
       if (this.pages.length > 0) {
         return {
           adress: this.pages[0].acf.adress,
@@ -59,54 +74,56 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .footer {
-    background-color: $blue !important;
+.footer {
+  background-color: $blue !important;
+}
+.lrFooter {
+  background-color: $blue !important;
+  height: 100%;
+  position: relative;
+  a,
+  p,
+  h4 {
+    color: white;
   }
-  .lrFooter {
-    background-color: $blue !important;
-    height: 100%;
-    position: relative;
-    a, p, h4 {
-      color: white;
-    }
-    a:hover {
-      text-decoration: underline;
-    }
+  a:hover {
+    text-decoration: underline;
+  }
+}
+.lrHeaderLine {
+  background-color: $red;
+}
+.footer-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-bottom: 200px;
+}
+.footer-item {
+  margin: 20px;
+  z-index: 300;
+}
+.footer-item:first-of-type p {
+  width: 50%;
+}
+.footer-icon-container {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  flex-flow: column wrap;
+}
+/* ****************** MEDIAQUERIES ****************** */
+@media screen and (max-width: $tablet - 1px) {
+  .footer-container {
+    margin-bottom: 0;
+    flex-direction: column;
   }
   .lrHeaderLine {
-    background-color: $red;
+    width: 70%;
   }
-  .footer-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin-bottom: 200px;
-  }
-  .footer-item {
-    margin: 20px;
-    z-index: 300;
-  }
-  .footer-item:first-of-type p {
-    width: 50%;
-  }
-  .footer-icon-container {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    display: flex;
-    flex-flow: column wrap;
-  }
-  /* ****************** MEDIAQUERIES ****************** */
-  @media screen and (max-width: $tablet - 1px) {
-    .footer-container {
-      margin-bottom: 0;
-      flex-direction: column;
-    }
-    .lrHeaderLine {
-      width: 70%;
-    }
-  }
-  @media screen and (min-width: $tablet) {
-  }
+}
+@media screen and (min-width: $tablet) {
+}
 </style>
--- src/components/lrHeader.vue
+++ src/components/lrHeader.vue
@@ -9,7 +9,6 @@
         <div class="lrHeader container">
           <h1>{{ this.headerContent.headingOne }}</h1>
           <hr class="lrHeaderLineHeader" />
-          <!-- <a href="/tjanster" id="lr-button-more" class="button is-info is-outlined">LÄS MER</a> -->
         </div>
       </div>
     </swiper-slide>
@@ -21,7 +20,18 @@
         <div class="lrHeader container">
           <h2>{{ this.headerContent.headingTwo }}</h2>
           <hr class="lrHeaderLineHeader" />
-          <!-- <a href="/online" id="lr-button-more" class="button is-info is-outlined">LÄS MER</a> -->
+          <p class="supportText">{{ this.headerContent.supportText }}</p>
+        </div>
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div
+        class="section headerThree header overlay"
+        v-bind:style="{ 'background-image': 'url(' + this.headerContent.imageThree.sizes.large + ')' }"
+      >
+        <div class="lrHeader container">
+          <h2>{{ this.headerContent.headingThree }}</h2>
+          <hr class="lrHeaderLineHeader" />
         </div>
       </div>
     </swiper-slide>
@@ -57,13 +67,18 @@ export default {
         return {
           headingOne: this.pages[5].acf.heading_one,
           headingTwo: this.pages[5].acf.heading_two,
+          headingThree: this.pages[5].acf.heading_three,
+          supportText: this.pages[5].acf.supporttext,
           imageOne: this.pages[5].acf.image_one,
-          imageTwo: this.pages[5].acf.image_two
+          imageTwo: this.pages[5].acf.image_two,
+          imageThree: this.pages[5].acf.image_three
         }
       } else {
         return {
           headingOne: '...',
           headingTwo: '...',
+          headingThree: '...',
+          supportText: '...',
           imageOne: {
             sizes: {
               large: '...'
@@ -73,18 +88,34 @@ export default {
             sizes: {
               large: '...'
             }
+          },
+          imageThree: {
+            sizes: {
+              large: '...'
+            }
           }
         }
       }
     }
   },
-  mounted() {}
+  mounted() {
+    console.log(this.pages)
+  }
 }
 </script>
 <style lang="scss" scoped>
 h1,
 h2 {
   text-transform: uppercase;
+  width: 75%;
+}
+.supportText {
+  @media #{$ipads} {
+    font-size: 1rem;
+  }
+  text-transform: none;
+  color: white;
+  text-align: center;
   width: 75%;
 }
 </style>
@@ -134,7 +165,8 @@ h2 {
 }
 @media #{$ipads} {
   .lrHeader h1,
-  .lrHeader h2 {
+  .lrHeader h2,
+  .lrHeader .supportText {
     width: 80%;
   }
 }
